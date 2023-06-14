ab29301688d0aabe2a41c228a2283a0d6e26d3f2
lroyero333 <lroyero@unab.edu.co>
2023-06-14 12:09:22 -0500

    Update module of projects and correct task

--- main/templates/calendario/cursos.py
+++ main/templates/calendario/cursos.py
@@ -12,20 +12,21 @@ from main.run import (agregar_tiempo_transcurrido, app, bcrypt, flash,
                       generarID, jsonify, mysql, redirect, render_template,
                       request, session, url_for)
 
-conexion = mysql.connect()
-cursor = conexion.cursor()
 
 @app.route('/cursos')
 def cursos():
     if not 'login' in session:
         return redirect('/')
-    fecha_actual=datetime.now()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    fecha_actual = datetime.now()
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("SELECT cursos.*, general_users.Nombre, general_users.Apellido, general_users.foto FROM cursos LEFT JOIN general_users ON cursos.id_usuario_fk = general_users.usuario ORDER BY fecha_publicacion DESC;")
     datosCursos = cursor.fetchall()
-    #Eliminar cuando se agregue correctamente en la base de datos
-    cursor.execute("SELECT * FROM cursos WHERE fecha_fin < %s", (fecha_actual,))
+    # Eliminar cuando se agregue correctamente en la base de datos
+    cursor.execute("SELECT * FROM cursos WHERE fecha_fin < %s",
+                   (fecha_actual,))
     datos_a_eliminar = cursor.fetchall()
     for dato in datos_a_eliminar:
         cursor.execute("DELETE FROM cursos WHERE id_curso = %s", (dato[0],))
@@ -33,25 +34,30 @@ def cursos():
     cursos_con_tiempo = agregar_tiempo_transcurrido(datosCursos, 5)
     return render_template('calendario/templates/cursos/Cursos.html', datosCursos=cursos_con_tiempo)
 
+
 @app.route('/cursos/<string:curso_id>', methods=['GET', 'POST'])
 def verCursos(curso_id):
     if not 'login' in session:
         return redirect('/')
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     query = "SELECT *, DATE_FORMAT(fecha_inicio, %s) AS fecha_inicio1, DATE_FORMAT(fecha_fin, %s) AS fecha_fin2 FROM cursos WHERE id_curso = %s;"
     cursor.execute(query, ('%d %M %Y', '%d %M %Y', curso_id))
     datosCursos = cursor.fetchone()
-    query ="SELECT inscripcion_cursos.*, DATE_FORMAT(fecha_inscripcion_curso,%s ) AS fecha_inscripcion, DATE_FORMAT(fecha_inscripcion_curso, %s) AS hora_inscripcion, general_users.Nombre, general_users.segundo_nombre, general_users.Apellido, general_users.segundo_apellido, general_users.foto, general_users.usuario FROM inscripcion_cursos LEFT JOIN general_users ON inscripcion_cursos.id_usuario_fk = general_users.usuario WHERE id_curso_fk= %s;"
+    query = "SELECT inscripcion_cursos.*, DATE_FORMAT(fecha_inscripcion_curso,%s ) AS fecha_inscripcion, DATE_FORMAT(fecha_inscripcion_curso, %s) AS hora_inscripcion, general_users.Nombre, general_users.segundo_nombre, general_users.Apellido, general_users.segundo_apellido, general_users.foto, general_users.usuario FROM inscripcion_cursos LEFT JOIN general_users ON inscripcion_cursos.id_usuario_fk = general_users.usuario WHERE id_curso_fk= %s;"
     cursor.execute(query, ('%d-%m-%Y', '%H:%i %p', curso_id))
     datosCursosInscritos = cursor.fetchall()
     # Buscar si el usuario actual está inscrito en el curso
-    cursor.execute("SELECT * FROM inscripcion_cursos WHERE id_usuario_fk = %s AND id_curso_fk = %s", (session['usuario'], curso_id))
+    cursor.execute("SELECT * FROM inscripcion_cursos WHERE id_usuario_fk = %s AND id_curso_fk = %s",
+                   (session['usuario'], curso_id))
     resultado = cursor.fetchone()
     conexion.commit()
 
     if request.method == 'POST':
         if resultado is not None:
             # Eliminar al usuario de la tabla de inscripción de cursos
-            cursor.execute("DELETE FROM inscripcion_cursos WHERE id_usuario_fk=%s AND id_curso_fk=%s", (session['usuario'], curso_id))
+            cursor.execute(
+                "DELETE FROM inscripcion_cursos WHERE id_usuario_fk=%s AND id_curso_fk=%s", (session['usuario'], curso_id))
             conexion.commit()
             flash("Has cancelado la inscripción en este curso.", 'correcto')
         else:
@@ -61,7 +67,8 @@ def verCursos(curso_id):
             fecha_inscripcion_curso = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
             query = "INSERT INTO inscripcion_cursos (id_inscripcion_cursos,id_usuario_fk, id_curso_fk, fecha_inscripcion_curso) VALUES (%s, %s, %s, %s)"
-            params = [generarID() ,id_usuario_fk, id_curso_fk, fecha_inscripcion_curso]
+            params = [generarID(), id_usuario_fk, id_curso_fk,
+                      fecha_inscripcion_curso]
 
             cursor.execute(query, params)
             conexion.commit()
@@ -70,10 +77,13 @@ def verCursos(curso_id):
 
         return redirect(f"/cursos/{curso_id}")
 
-    return render_template('calendario/templates/cursos/verCurso.html', datosCursos=datosCursos, inscrito=(resultado is not None),datosCursosInscritos=datosCursosInscritos)
+    return render_template('calendario/templates/cursos/verCurso.html', datosCursos=datosCursos, inscrito=(resultado is not None), datosCursosInscritos=datosCursosInscritos)
+
 
 @app.route('/curso/crear', methods=['GET', 'POST'])
 def crearCurso():
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     if request.method == 'POST':
         if 'crear_curso' in request.form:
             id_usuario_fk = session["usuario"]
@@ -83,32 +93,38 @@ def crearCurso():
             ubicacion = request.form['ubicacion_curso']
             descripcion = request.form['descripcion']
             descripcion_corta = request.form['descripcion_corta']
-            horario=request.form['horario_curso']
+            horario = request.form['horario_curso']
             fecha_publicacion = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
             query = "INSERT INTO cursos (id_curso,id_usuario_fk,nombre_curso, fecha_inicio, fecha_fin, fecha_publicacion, ubicacion, descripcion, descripcion_corta, horario) VALUES (%s, %s,%s,%s, %s, %s, %s, %s, %s, %s)"
-            params = [generarID() , id_usuario_fk, nombre_curso, fecha_inicio_curso,
-                      Fecha_fin_curso, fecha_publicacion, ubicacion, descripcion,descripcion_corta, horario]
+            params = [generarID(), id_usuario_fk, nombre_curso, fecha_inicio_curso,
+                      Fecha_fin_curso, fecha_publicacion, ubicacion, descripcion, descripcion_corta, horario]
 
             cursor.execute(query, params)
             conexion.commit()
-            flash('Curso agregado satisfactoriamente','correcto')
+            flash('Curso agregado satisfactoriamente', 'correcto')
 
             return redirect('/calendario')
     return render_template('calendario/templates/cursos/crearCursos.html')
 
+
 @app.route('/cursos/lista/editar', methods=['GET', 'POST'])
 def listaCursoEditar():
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     if request.method == 'POST':
         curso_id = request.form.get('curso_id')
         if request.form.get('editar_curso'):
             return redirect(f"/curso/editarCurso/{curso_id}")
     cursor.execute("SELECT * FROM cursos")
     cursos = cursor.fetchall()
-    return render_template('calendario/templates/cursos/listaEditarCursos.html',cursos=cursos)
+    return render_template('calendario/templates/cursos/listaEditarCursos.html', cursos=cursos)
+
 
 @app.route('/cursos/lista/eliminar', methods=['GET', 'POST'])
 def listaCursoEliminar():
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     if request.method == 'POST':
         curso_id = request.form.get('curso_id')
         if request.form.get('borrar_curso'):
@@ -119,7 +135,8 @@ def listaCursoEliminar():
             return redirect('/calendario')
     cursor.execute("SELECT * FROM cursos")
     cursos = cursor.fetchall()
-    return render_template('calendario/templates/cursos/listaEliminarCursos.html',cursos=cursos)
+    return render_template('calendario/templates/cursos/listaEliminarCursos.html', cursos=cursos)
+
 
 @app.route('/curso/editarCurso/<string:curso_id>', methods=['GET', 'POST'])
 def editCurso(curso_id):
@@ -127,10 +144,11 @@ def editCurso(curso_id):
         return redirect('/')
     if session['cargo'] != 1:
         return redirect('/inicio')
-    
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM cursos WHERE id_curso= %s", curso_id)
     curso = cursor.fetchone()
-    
+
     if request.method == 'POST':
         # Obtener los valores de los campos del formulario
         id_usuario_fk = session["usuario"]
@@ -153,5 +171,5 @@ def editCurso(curso_id):
 
         flash('El curso ha sido editado correctamente.', 'correcto')
         return redirect('/calendario')
-    
+
     return render_template('calendario/templates/cursos/editarCurso.html', curso=curso)
--- main/templates/calendario/noticias.py
+++ main/templates/calendario/noticias.py
@@ -14,33 +14,38 @@ from main.run import (agregar_tiempo_transcurrido, app, bcrypt, flash,
                       generarID, jsonify, mysql, redirect, render_template,
                       request, session, stringAleatorio, url_for)
 
-extensionesImagenes=['.jpg', '.jpeg', '.png']
+extensionesImagenes = ['.jpg', '.jpeg', '.png']
 conexion = mysql.connect()
 cursor = conexion.cursor()
+
+
 def stringAleatorio():
-    string_aleatorio = "0123456789abcdefghijklmnñopqrstuvwxyz_"
+    string_aleatorio = "0123456789abcdefghijklmnopqrstuvwxyz_"
     longitud = 10
     secuencia = string_aleatorio.upper()
     resultado_aleatorio = sample(secuencia, longitud)
     string_aleatorio = "".join(resultado_aleatorio)
     return string_aleatorio
 
+
 @app.route('/noticias')
 def noticias():
     if not 'login' in session:
         return redirect('/')
-    fecha_actual=datetime.now()
+    fecha_actual = datetime.now()
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("SELECT noticias.*, general_users.Nombre, general_users.Apellido, general_users.foto FROM noticias LEFT JOIN general_users ON noticias.id_usuario_fk = general_users.usuario ORDER BY fecha_publicacion DESC;")
     datosNoticias = cursor.fetchall()
     fecha_limite = fecha_actual - dt.timedelta(days=6 * 30)
     cursor = conexion.cursor()
-    cursor.execute("DELETE FROM noticias WHERE fecha_publicacion < %s", (fecha_limite,))
+    cursor.execute(
+        "DELETE FROM noticias WHERE fecha_publicacion < %s", (fecha_limite,))
 
     conexion.commit()
     noticias_con_tiempo = agregar_tiempo_transcurrido(datosNoticias, 4)
     return render_template('calendario/templates/noticias/Noticias.html',  datosNoticias=noticias_con_tiempo)
+
 
 @app.route('/noticia/editarNoticia/<string:noticia_id>', methods=['GET', 'POST'])
 def editNoticia(noticia_id):
@@ -50,30 +55,34 @@ def editNoticia(noticia_id):
         return redirect('/inicio')
     cursor.execute("SELECT * FROM noticias WHERE id_noticia= %s", noticia_id)
     noticia = cursor.fetchone()
-    
+
     if request.method == 'POST':
         # Obtener los valores de los campos del formulario
 
         usuarioPublica = session["usuario"]
         titulo_noticia = request.form.get('titulo_noticia') or noticia[1]
-        
-        descripcion_noticia = request.form.get('descripcion_noticia') or noticia[3]
+
+        descripcion_noticia = request.form.get(
+            'descripcion_noticia') or noticia[3]
         fecha_publicacion = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         if request.files['imagen_noticia'].filename != '':
             imagen_noticia = request.files['imagen_noticia']
-            filename, file_extension = os.path.splitext(imagen_noticia.filename)
+            filename, file_extension = os.path.splitext(
+                imagen_noticia.filename)
             if file_extension.lower() not in extensionesImagenes:
-                flash('La extensión de la imagen no está permitida. Solo se permiten archivos JPG, JPEG y PNG.','error')
+                flash(
+                    'La extensión de la imagen no está permitida. Solo se permiten archivos JPG, JPEG y PNG.', 'error')
             else:
-                flash('La noticia se ha actualizado satisfactoriamente','correcto')
+                flash('La noticia se ha actualizado satisfactoriamente', 'correcto')
             basepath = os.path.dirname(__file__)
             filename = secure_filename(imagen_noticia.filename)
 
             extension = os.path.splitext(filename)[1]
             nuevoNombreImagen = stringAleatorio() + extension
 
-            upload_path = os.path.join( basepath, app.root_path, 'static', 'images', 'Noticias', nuevoNombreImagen)
+            upload_path = os.path.join(
+                basepath, app.root_path, 'static', 'images', 'Noticias', nuevoNombreImagen)
             if not os.path.exists(os.path.dirname(upload_path)):
                 os.makedirs(os.path.dirname(upload_path))
 
@@ -84,13 +93,15 @@ def editNoticia(noticia_id):
             nuevoNombreImagen = noticia[2]
 
         query = "UPDATE noticias SET id_usuario_fk = %s, titulo_noticia = %s, imagen_noticia = %s, descripcion_noticia = %s,fecha_publicacion = %s WHERE id_noticia = %s"
-        params = [usuarioPublica, titulo_noticia, nuevoNombreImagen,descripcion_noticia, fecha_publicacion, noticia_id]
+        params = [usuarioPublica, titulo_noticia, nuevoNombreImagen,
+                  descripcion_noticia, fecha_publicacion, noticia_id]
         cursor.execute(query, params)
         conexion.commit()
         flash('El curso ha sido editado.', 'success')
         return redirect('/calendario')
-    
+
     return render_template('calendario/templates/noticias/editarNoticia.html', noticia=noticia)
+
 
 @app.route('/noticias/crear', methods=['GET', 'POST'])
 def crearNoticia():
@@ -101,12 +112,14 @@ def crearNoticia():
             imagen_noticia = request.files['imagen_noticia']
             descripcion_noticia = request.form['descripcion_noticia']
             fecha_publicacion = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            filename, file_extension = os.path.splitext(imagen_noticia.filename)
+            filename, file_extension = os.path.splitext(
+                imagen_noticia.filename)
             if file_extension.lower() not in extensionesImagenes:
-                flash('La extensión de la imagen no está permitida. Solo se permiten archivos JPG, JPEG y PNG.','error')
+                flash(
+                    'La extensión de la imagen no está permitida. Solo se permiten archivos JPG, JPEG y PNG.', 'error')
                 return redirect('/calendario')
             else:
-                flash('La noticia se ha agregado satisfactoriamente','correcto')
+                flash('La noticia se ha agregado satisfactoriamente', 'correcto')
             basepath = os.path.dirname(__file__)
             filename = secure_filename(imagen_noticia.filename)
             extension = os.path.splitext(filename)[1]
@@ -118,7 +131,7 @@ def crearNoticia():
 
             imagen_noticia.save(upload_path)
             query = "INSERT INTO noticias (id_noticia, titulo_noticia, imagen_noticia, descripcion_noticia, fecha_publicacion,id_usuario_fk) VALUES (%s, %s,%s, %s, %s,%s)"
-            params = [generarID() ,titulo_noticia, nuevoNombreImagen,
+            params = [generarID(), titulo_noticia, nuevoNombreImagen,
                       descripcion_noticia, fecha_publicacion, usuarioPublica]
 
             cursor.execute(query, params)
@@ -128,39 +141,42 @@ def crearNoticia():
 
     return render_template('calendario/templates/noticias/crearNoticias.html')
 
+
 @app.route('/noticias/lista/editar', methods=['GET', 'POST'])
 def listaNoticiaEditar():
     conexion = mysql.connect()
     cursor = conexion.cursor()
-    if request.method == 'POST':  
+    if request.method == 'POST':
         noticia_id = request.form.get('noticia_id')
         if request.form.get('editar_noticia'):
             return redirect(f"/noticia/editarNoticia/{noticia_id}")
     cursor.execute("SELECT * FROM noticias")
     noticias = cursor.fetchall()
     conexion.close()
-    return render_template('calendario/templates/noticias/listaEditarNoticias.html',noticias=noticias)
+    return render_template('calendario/templates/noticias/listaEditarNoticias.html', noticias=noticias)
+
 
 @app.route('/noticia/lista/eliminar', methods=['GET', 'POST'])
 def listaNoticiaEliminar():
-    if request.method == 'POST':        
+    if request.method == 'POST':
         noticia_id = request.form.get('noticia_id')
         if request.form.get('borrar_noticia'):
             cursor.execute(
                 "SELECT imagen_noticia FROM noticias WHERE id_noticia = %s;", (noticia_id,))
-            nombre_archivo=cursor.fetchone()
+            nombre_archivo = cursor.fetchone()
             print('NOMBREARCHIVO', nombre_archivo[0])
-            ruta_archivo = os.path.join(app.root_path, 'static', 'images', 'Noticias', nombre_archivo[0])
+            ruta_archivo = os.path.join(
+                app.root_path, 'static', 'images', 'Noticias', nombre_archivo[0])
             cursor.execute(
                 "DELETE FROM noticias WHERE id_noticia = %s;", (noticia_id,))
             conexion.commit()
             if os.path.exists(ruta_archivo):
                 os.remove(ruta_archivo)
-            flash('La noticia ha sido eliminada correctamente','correcto')
+            flash('La noticia ha sido eliminada correctamente', 'correcto')
             return redirect('/calendario')
     cursor.execute("SELECT * FROM noticias")
     noticias = cursor.fetchall()
-    return render_template('calendario/templates/noticias/listaEliminarNoticias.html',noticias=noticias)
+    return render_template('calendario/templates/noticias/listaEliminarNoticias.html', noticias=noticias)
 
 
 @app.route('/noticia/<string:noticia_id>', methods=['GET', 'POST'])
