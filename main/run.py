2b7c1120329bb1da33b0d5e2fa0c983ea9bda1d5
lroyero333 <lroyero@unab.edu.co>
2023-06-16 11:44:31 -0500

    Update module of permision and correct grammar

--- main/run.py
+++ main/run.py
@@ -12,7 +12,7 @@ app=Flask(__name__, static_url_path='/static')
 app.config['DEBUG'] = True
 app.secret_key="18*=70Y3DugTJ;-~&Jnr"
 mysql=MySQL()
-
+"""
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']=''
@@ -22,7 +22,7 @@ app.config['MYSQL_DATABASE_HOST']='34.16.128.53'
 app.config['MYSQL_DATABASE_USER']='usuarioHB'
 app.config['MYSQL_DATABASE_PASSWORD']='Human100.'
 app.config['MYSQL_DATABASE_DB']='IntranetHB'
-"""
+
 """app.config['MYSQL_DATABASE_HOST']='srv652.hstgr.io'
 app.config['MYSQL_DATABASE_USER']='u122395259_HumanBionics'
 app.config['MYSQL_DATABASE_PASSWORD']='Human100.'
